e1296e124c2299f12e0b2098503762868da33a7b
jugglingcats <github@akirkpatrick.com>
2022-02-09 11:33:12 +0000

    Fix to fault state handling

--- libs/store/src/machine/MachineStateHandler.ts
+++ libs/store/src/machine/MachineStateHandler.ts
@@ -20,7 +20,9 @@ export enum DesiredState {
 
 export function determine_machine_state(status: number): MachineState {
     if (status & 0b1000) {
-        return (status & 0b1111) === 0b1111 ? MachineState.FAULT_REACTION_ACTIVE : MachineState.FAULT
+        return (status & 0b1111) === 0b1111
+            ? MachineState.FAULT_REACTION_ACTIVE
+            : MachineState.FAULT
     }
 
     if ((status & 0b01001111) === 0b01000000) {
@@ -52,57 +54,70 @@ export const possible_transitions = {
     QuickStop: () => 0b00000010
 }
 
-export function handleMachineState(currentState: MachineState, controlWord: number, desiredState: DesiredState): number | undefined {
+export function handleMachineState(
+    currentState: MachineState,
+    controlWord: number,
+    desiredState: DesiredState
+): number | undefined {
     const state = currentState
-    if (desiredState === DesiredState.OPERATIONAL) {
-        // console.log("HANDLE MACHINE STATE - OPERATION ENABLED - CURRENT", state)
-        switch (state) {
-            case MachineState.UNKNOWN:
-            case MachineState.FAULT_REACTION_ACTIVE:
-            case MachineState.FAULT:
-            case MachineState.NOT_READY_TO_SWITCH_ON:
-            case MachineState.OPERATION_ENABLED:
-            case MachineState.QUICK_STOP:
-                break
-            case MachineState.SWITCH_ON_DISABLED:
-                // console.log("TRANSITION SHUTDOWN")
-                return possible_transitions.Shutdown()
-            case MachineState.READY_TO_SWITCH_ON:
-                // console.log("TRANSITION SWITCH ON")
-                return possible_transitions.SwitchOn()
-            case MachineState.SWITCHED_ON:
-                // console.log("TRANSITION ENABLE")
-                return possible_transitions.EnableOperation()
-        }
-    } else if (desiredState === DesiredState.STANDBY) {
-        // console.log("HANDLE MACHINE STATEd - STANDBY - CURRENT", state, "DESIRED", desiredState)
-        switch (state) {
-            case MachineState.SWITCH_ON_DISABLED:
-                if (controlWord === possible_transitions.FaultReset()) {
-                    // clear the fault reset command
+    switch (desiredState) {
+        case DesiredState.OPERATIONAL:
+            switch (state) {
+                case MachineState.UNKNOWN:
+                case MachineState.FAULT_REACTION_ACTIVE:
+                case MachineState.FAULT:
+                case MachineState.NOT_READY_TO_SWITCH_ON:
+                case MachineState.OPERATION_ENABLED:
+                case MachineState.QUICK_STOP:
+                    break
+                case MachineState.SWITCH_ON_DISABLED:
+                    // console.log("TRANSITION SHUTDOWN")
+                    return possible_transitions.Shutdown()
+                case MachineState.READY_TO_SWITCH_ON:
+                    // console.log("TRANSITION SWITCH ON")
+                    return possible_transitions.SwitchOn()
+                case MachineState.SWITCHED_ON:
+                    // console.log("TRANSITION ENABLE")
+                    return possible_transitions.EnableOperation()
+            }
+            break
+        case DesiredState.STANDBY:
+            switch (state) {
+                case MachineState.SWITCH_ON_DISABLED:
+                    if (controlWord === possible_transitions.FaultReset()) {
+                        // clear the fault reset command
+                        return possible_transitions.DisableVoltage()
+                    }
+                    break
+                case MachineState.UNKNOWN:
+                    break
+                case MachineState.FAULT_REACTION_ACTIVE:
+                    break
+                case MachineState.FAULT:
+                    break
+                case MachineState.NOT_READY_TO_SWITCH_ON:
+                    break
+                case "OPERATION_ENABLED":
                     return possible_transitions.DisableVoltage()
-                }
-                break
-            case MachineState.UNKNOWN:
-                break
-            case MachineState.FAULT_REACTION_ACTIVE:
-                break
-            case MachineState.FAULT:
-
-                break
-            case MachineState.NOT_READY_TO_SWITCH_ON:
-                break
-            case "OPERATION_ENABLED":
+                case MachineState.QUICK_STOP:
+                    break
+                case MachineState.SWITCHED_ON:
+                    break
+                case MachineState.READY_TO_SWITCH_ON:
+                    return possible_transitions.DisableVoltage()
+                default:
+                    break
+            }
+            break
+        case DesiredState.NONE:
+            if (
+                state === MachineState.SWITCH_ON_DISABLED &&
+                controlWord === possible_transitions.FaultReset()
+            ) {
+                // clear the fault reset command
                 return possible_transitions.DisableVoltage()
-            case MachineState.QUICK_STOP:
-                break
-            case MachineState.SWITCHED_ON:
-                break
-            case MachineState.READY_TO_SWITCH_ON:
-                return possible_transitions.DisableVoltage()
-            default:
-                break
-        }
+            }
+            break
     }
     return undefined
 }
