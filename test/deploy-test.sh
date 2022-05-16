2c4bbadf0e2a7565dd6591691536d35909de995d
jugglingcats <github@akirkpatrick.com>
2022-05-16 08:37:45 +0100

    Fix deploy-test.sh

--- test/deploy-test.sh
+++ test/deploy-test.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-ls -l
-stat ./test.js
-
-aws s3 sync ./test.js "s3://$1/gbr/test.js" && \
+aws s3 cp ./test.js "s3://$1/gbr/test.js" && \
 aws cloudfront create-invalidation --paths "/*" --distribution-id $2
