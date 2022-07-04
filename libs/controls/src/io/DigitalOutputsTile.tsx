d5ec4e797ddf4b4cec584646a81f17471bb681b0
jugglingcats <github@akirkpatrick.com>
2022-07-04 16:49:22 +0100

    fix(#12): Digital Outputs tile - id not displayed

--- libs/controls/src/io/DigitalOutputsTile.tsx
+++ libs/controls/src/io/DigitalOutputsTile.tsx
@@ -12,13 +12,20 @@ const help = (
     <div>
         <h4>Digital Outputs Tile</h4>
         <p>The Digital Outputs Tile allows you to control analog outputs on your machine.</p>
-        <p>If you want your outputs to be controlled by your programs (or gcode) then set "Auto" mode in the dropdown. </p>
-        <p>If you want to force the value of the output from the Tile, set "Override" mode in the dropdown.</p>
+        <p>
+            If you want your outputs to be controlled by your programs (or gcode) then set "Auto"
+            mode in the dropdown.{" "}
+        </p>
+        <p>
+            If you want to force the value of the output from the Tile, set "Override" mode in the
+            dropdown.
+        </p>
         <p>In overide mode the value can be changed with the toggle switch.</p>
-        <p>The current value of the output will be shown in the state indicator on the far right.</p>
+        <p>
+            The current value of the output will be shown in the state indicator on the far right.
+        </p>
     </div>
 )
-
 
 const { Option } = Select
 
@@ -97,7 +104,7 @@ export const DigitalOutputsTile = ({ labels = [] }: DigitalOutputsTileProps) => 
         <Tile title={"Digital Outputs"} help={help}>
             <StyledDiv>
                 {douts.map((name, index) => (
-                    <DigitalOutputItem key={index} index={index} label={labels[index]} />
+                    <DigitalOutputItem key={index} index={index} label={labels[index] || name} />
                 ))}
             </StyledDiv>
         </Tile>
