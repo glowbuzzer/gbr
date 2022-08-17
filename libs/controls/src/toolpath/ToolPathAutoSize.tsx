0310f9c3ad8445d7756395ec63e8034f8e0d43e7
jugglingcats <github@akirkpatrick.com>
2022-08-17 10:56:07 +0100

    fix(#23): Fix orbit controls issue

--- libs/controls/src/toolpath/ToolPathAutoSize.tsx
+++ libs/controls/src/toolpath/ToolPathAutoSize.tsx
@@ -2,15 +2,14 @@
  * Copyright (c) 2022. Glowbuzzer. All rights reserved
  */
 
-import { extend, useThree } from "@react-three/fiber"
+import { useThree } from "@react-three/fiber"
 import * as React from "react"
-import { useEffect } from "react"
-import { OrbitControls } from "three-stdlib"
-
-extend({ OrbitControls })
+import { useEffect, useState } from "react"
+import { OrbitControls } from "@react-three/drei"
 
 /** @ignore - internal to the tool path tile */
 export const ToolPathAutoSize = ({ extent, children }) => {
+    const [controls, setControls] = useState(null)
     const {
         camera,
         gl: { domElement }
@@ -20,14 +19,16 @@ export const ToolPathAutoSize = ({ extent, children }) => {
         camera.position.z = 2 * extent
         camera.near = 0.01
         camera.far = 10000
-
+        camera.up.set(0, 0, 1)
         camera.updateProjectionMatrix()
+
+        // controls need to be created _after_ setting the camera 'up' property!
+        setControls(<OrbitControls camera={camera} enableDamping={false} />)
     }, [extent])
 
     return (
         <>
-            {/* @ts-ignore */}
-            <orbitControls args={[camera, domElement]} />
+            {controls}
             {children}
         </>
     )
