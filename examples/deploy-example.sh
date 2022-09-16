e4c23954f5c9922e22ab324baeca92bd24ba7b83
jugglingcats <github@akirkpatrick.com>
2022-09-16 11:55:03 +0100

    chore: fix example deploy

--- examples/deploy-example.sh
+++ examples/deploy-example.sh
@@ -4,5 +4,5 @@
 # Copyright (c) 2022. Glowbuzzer. All rights reserved
 #
 
-aws s3 sync --region eu-west-1 --delete ./dist/examples/$1 "s3://$2/examples/$1" && \
+aws s3 sync --region eu-west-1 --delete ./examples/$1/dist "s3://$2/examples/$1" && \
 aws cloudfront create-invalidation --region eu-west-1 --paths "/*" --distribution-id $3
