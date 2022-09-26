6b2eb96e2ab94f5b0f0d3a641a5518adbd5b8d1d
jugglingcats <github@akirkpatrick.com>
2022-09-26 09:20:56 +0100

    feat(#35): Add pre-canned views (cube) to toolpath display

--- libs/controls/src/toolpath/ToolPathAutoSize.tsx
+++ libs/controls/src/toolpath/ToolPathAutoSize.tsx
@@ -5,7 +5,7 @@
 import { useThree } from "@react-three/fiber"
 import * as React from "react"
 import { useEffect, useState } from "react"
-import { OrbitControls } from "@react-three/drei"
+import { GizmoHelper, GizmoViewcube, OrbitControls, PerspectiveCamera } from "@react-three/drei"
 
 /** @ignore - internal to the tool path tile */
 export const ToolPathAutoSize = ({ extent, children }) => {
@@ -15,20 +15,52 @@ export const ToolPathAutoSize = ({ extent, children }) => {
         gl: { domElement }
     } = useThree()
 
-    useEffect(() => {
-        camera.position.z = 2 * extent
-        camera.near = 0.01
-        camera.far = 10000
-        camera.up.set(0, 0, 1)
-        camera.updateProjectionMatrix()
+    // useEffect(() => {
+    //     camera.position.z = 2 * extent
+    //     camera.near = 0.01
+    //     camera.far = 10000
+    //     camera.up.set(0, 0, 1)
+    //     camera.updateProjectionMatrix()
+    //
+    //     // controls need to be created _after_ setting the camera 'up' property!
+    //     setControls(
+    //         <>
+    //             <OrbitControls camera={camera} enableDamping={false} />
+    //             <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+    //                 <GizmoViewcube
+    //                     {...{
+    //                         faces: ["Right", "Left", "Back", "Front", "Top", "Bottom"]
+    //                     }}
+    //                 />
+    //                 )
+    //             </GizmoHelper>
+    //         </>
+    //     )
+    // }, [extent])
 
-        // controls need to be created _after_ setting the camera 'up' property!
-        setControls(<OrbitControls camera={camera} enableDamping={false} />)
-    }, [extent])
+    console.log("Extent: ", extent)
 
     return (
         <>
+            <PerspectiveCamera
+                makeDefault
+                position={[0, 0, 3 * extent]}
+                far={10000}
+                near={0.01}
+                up={[0, 0, 1]}
+            />
+            <OrbitControls camera={camera} enableDamping={false} />
+            <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+                <GizmoViewcube
+                    {...{
+                        faces: ["Right", "Left", "Back", "Front", "Top", "Bottom"]
+                    }}
+                />
+                )
+            </GizmoHelper>
+            {/*
             {controls}
+*/}
             {children}
         </>
     )
