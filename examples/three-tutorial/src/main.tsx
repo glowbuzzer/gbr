a46c34ea83bca187ad4a5c0cbfa6948594a0793d
jugglingcats <github@akirkpatrick.com>
2022-11-14 12:45:48 +0000

    chore: refactor three-tutorial in line with robot rendering changes

--- examples/three-tutorial/src/main.tsx
+++ examples/three-tutorial/src/main.tsx
@@ -1,115 +1,87 @@
 import * as React from "react"
-import {
-    useState,
-    StrictMode,
-    useRef,
-    useEffect,
-    useContext,
-    createContext,
-    Suspense,
-    forwardRef,
-    useLayoutEffect
-} from "react"
-import * as THREE from 'three'
-import {Switch, Space} from "antd"
-import {Physics, useBox, useSphere, usePlane} from "@react-three/cannon"
+import { StrictMode, Suspense, useContext, useState } from "react"
+
+import { Html, useGLTF } from "@react-three/drei"
 
 import {
-    OrbitControls,
-    Line,
-    Sphere,
-    Box,
-    Html,
-    Text,
-    PerspectiveCamera,
-    Points,
-    Point,
-    PointMaterial,
-    PivotControls,
-    useTexture,
-    useHelper,
-    Stage,
-    Plane,
-} from "@react-three/drei"
-
-import {useLoader, useFrame, useThree} from "@react-three/fiber"
-
-
-import {
+    BasicRobot,
     DockLayout,
-    DockLayoutProvider, DockTileDefinition,
+    DockLayoutProvider,
+    DockTileDefinition,
     DockTileDefinitionBuilder,
     GlowbuzzerApp,
-    GlowbuzzerTileDefinitions, GlowbuzzerTileIdentifiers, JogJointsTile,
-    RobotModel,
+    GlowbuzzerTileDefinitions,
+    RobotKinematicsChainElement,
     ThreeDimensionalSceneTile
 } from "@glowbuzzer/controls"
-
-
-import {Vector3} from "three"
-import {createRoot} from "react-dom/client"
-import {ExampleAppMenu} from "../../util/ExampleAppMenu"
+import { createRoot } from "react-dom/client"
+import { ExampleAppMenu } from "../../util/ExampleAppMenu"
 
 import "antd/dist/antd.css"
 import "dseg/css/dseg.css"
 import "flexlayout-react/style/light.css"
-import {appContext} from "../../convmc/src/app/AppContextType";
 
-import {ChooseExample} from "./menuTile"
-import {ExampleSphere} from "./exampleSphere"
-import {ExampleTexture} from "./exampleTexture"
-import {ExampleSpotlightOnObject} from "./exampleSpotlightOnObject"
-import {ExamplePhysics} from "./examplePhysics"
-import {ExampleSprites} from "./exampleSprites"
-import {ExampleIndicators} from "./exampleIndicators"
-import {ExampleShowObjectCoordinates} from "./exampleShowObjectCoordinates"
-import {ExampleMoveObject} from "./exampleMoveObject";
-import {ExampleCollisionDetection} from "./exampleCollisionDetection"
-import {ExampleGripper} from "./exampleGripper"
-import {ExampleSpring} from "./exampleSpring"
-import {ExamplePendulum} from "./examplePendulum"
-import {ExampleUI} from "./ExampleUI";
-import {ExampleTriadPoints} from "./ExampleTriadPoints"
+import { ChooseExample } from "./menuTile"
+import { ExampleSphere } from "./exampleSphere"
+import { ExampleTexture } from "./exampleTexture"
+import { ExampleSpotlightOnObject } from "./exampleSpotlightOnObject"
+import { ExamplePhysics } from "./examplePhysics"
+import { ExampleSprites } from "./exampleSprites"
+import { ExampleIndicators } from "./exampleIndicators"
+import { ExampleShowObjectCoordinates } from "./exampleShowObjectCoordinates"
+import { ExampleMoveObject } from "./exampleMoveObject"
+import { ExampleCollisionDetection } from "./exampleCollisionDetection"
+import { ExampleGripper } from "./exampleGripper"
+import { ExampleSpring } from "./exampleSpring"
+import { ExamplePendulum } from "./examplePendulum"
+import { ExampleUI } from "./ExampleUI"
+import { ExampleTriadPoints } from "./ExampleTriadPoints"
 
-import {ActiveExampleContext} from "./activeExampleContext";
-import {ExampleLaserCutting} from "./exampleLaserCutting";
+import { ActiveExampleContext } from "./activeExampleContext"
+import { ExampleLaserCutting } from "./exampleLaserCutting"
+import { useFrame, useJointPositions, useKinematicsConfiguration } from "@glowbuzzer/store"
+import { Vector3 } from "three"
 
-const ChooseExampleTileDefinition: DockTileDefinition =
-    {
-        id: "chooseExample",
-        name: "Choose Example",
-        defaultPlacement: {
-            column: 2,
-            row: 0,
-        },
-        excludeByDefault: false,
-        config: {
-            enableWithoutConnection: true
-        },
-        render: () => <ChooseExample/>
-    }
-
+const ChooseExampleTileDefinition: DockTileDefinition = {
+    id: "chooseExample",
+    name: "Choose Example",
+    defaultPlacement: {
+        column: 2,
+        row: 0
+    },
+    excludeByDefault: false,
+    config: {
+        enableWithoutConnection: true
+    },
+    render: () => <ChooseExample />
+}
 
 const DEG90 = Math.PI / 2
 
-const TX40_MODEL: RobotModel = {
-    name: "tx40",
-    config: [
-        {alpha: -DEG90, limits: [-270, 270]},
-        {alpha: 0, link_length: 0.225, teta: -DEG90, limits: [-270, 270]},
-        {alpha: DEG90, offset: 0.035, teta: DEG90, limits: [-270, 270]},
-        {alpha: -DEG90, offset: 0.225, limits: [-270, 270]},
-        {alpha: DEG90, limits: [-270, 270]},
-        {offset: 0.065, limits: [-270, 270]}
-    ],
-    offset: new THREE.Vector3(0, 0, 325),
-    scale: 1000
-}
+const TX40_KIN_CHAIN: RobotKinematicsChainElement[] = [
+    { moveable: true },
+    { rotateX: -DEG90, moveable: true, teta: -DEG90 },
+    { rotateX: 0, translateX: 0.225, teta: DEG90, moveable: true },
+    { rotateX: DEG90, translateZ: 0.035, moveable: true },
+    { rotateX: -DEG90, translateZ: 0.225, moveable: true },
+    { rotateX: DEG90, moveable: true },
+    { translateZ: 0.065 }
+]
+
+const DEFAULT_POSITION = new Vector3(0, 0, 325)
 
 const ThreeTutorial = () => {
+    const jointPositions = useJointPositions(0)
+    const { frameIndex } = useKinematicsConfiguration(0)
+    const { translation, rotation } = useFrame(frameIndex, false)
 
-    // const [showRobot, setShowRobot] = useState(true)
-    const maxExtent = (val) => {
+    const parts = useGLTF([0, 1, 2, 3, 4, 5, 6].map(j => `/assets/tx40/L${j}.glb`)).map(
+        m => m.scene
+    )
+
+    console.log("PARTS", parts)
+
+    const maxExtent = val => {
         // console.log(val);
     }
 
@@ -117,99 +89,115 @@ const ThreeTutorial = () => {
     // const orientation = useKinematicsCartesianPosition(0).position.rotation
     // console.log(position)
 
-    const {activeExample, setActiveExample} = useContext(ActiveExampleContext)
-    console.log("main", activeExample)
-
-    // exampleContent = <Html><h1>example1</h1></Html>
-    // exampleContent = <Html><h1>example2</h1></Html>
+    const { activeExample, setActiveExample } = useContext(ActiveExampleContext)
 
     let exampleContent
 
     switch (activeExample) {
         case 1: {
-            exampleContent = <ExampleSphere/>
+            exampleContent = <ExampleSphere />
             break
         }
         case 2: {
-            exampleContent = <ExampleTexture/>
+            exampleContent = <ExampleTexture />
             break
         }
         case 3: {
-            exampleContent = <ExampleSpotlightOnObject/>
+            exampleContent = <ExampleSpotlightOnObject />
             break
         }
         case 4: {
-            exampleContent = <ExamplePhysics/>
+            exampleContent = <ExamplePhysics />
             break
         }
         case 5: {
-            exampleContent = <ExampleSprites i={5} j={6}/>
-             break
+            exampleContent = <ExampleSprites i={5} j={6} />
+            break
         }
         case 6: {
-            exampleContent = <ExampleShowObjectCoordinates/>
+            exampleContent = <ExampleShowObjectCoordinates />
             break
         }
         case 7: {
-            exampleContent = <ExampleMoveObject/>
+            exampleContent = <ExampleMoveObject />
             break
         }
         case 8: {
-            exampleContent = <ExampleCollisionDetection/>
+            exampleContent = <ExampleCollisionDetection />
             break
         }
         case 9: {
-            exampleContent = <ExampleTriadPoints/>
+            exampleContent = <ExampleTriadPoints />
             break
         }
 
         case 10: {
-            exampleContent = <ExampleUI/>
+            exampleContent = <ExampleUI />
             break
         }
         case 11: {
-            exampleContent = <ExamplePendulum/>
+            exampleContent = <ExamplePendulum />
             break
         }
         case 12: {
-            exampleContent = <ExampleGripper/>
+            exampleContent = <ExampleGripper />
             break
         }
         case 13: {
-            exampleContent = <ExampleIndicators/>
+            exampleContent = <ExampleIndicators />
             break
         }
         case 14: {
-            exampleContent = <ExampleSpring/>
+            exampleContent = <ExampleSpring />
             break
         }
         case 15: {
-            exampleContent = <ExampleLaserCutting/>
+            exampleContent = <ExampleLaserCutting />
             break
         }
 
         default: {
-            exampleContent = <Html><h1>Error</h1></Html>
+            exampleContent = (
+                <Html>
+                    <h1>Error</h1>
+                </Html>
+            )
             break
         }
     }
-    return (<ThreeDimensionalSceneTile model={TX40_MODEL} noViewCube={true} noCamera={false} noGridHelper={false} noLighting={false}>{exampleContent}</ThreeDimensionalSceneTile>)
-    // return (<ThreeDimensionalSceneTile noViewCube={true} noCamera={false} noGridHelper={true} noLighting={true}>{exampleContent}</ThreeDimensionalSceneTile>)
+
+    return (
+        <ThreeDimensionalSceneTile noViewCube>
+            <BasicRobot
+                kinematicsChain={TX40_KIN_CHAIN}
+                jointPositions={jointPositions}
+                parts={parts}
+                translation={translation || DEFAULT_POSITION}
+                rotation={rotation}
+                scale={1000}
+            />
+            {exampleContent}
+        </ThreeDimensionalSceneTile>
+    )
 }
 
 const CustomSceneTile = DockTileDefinitionBuilder(GlowbuzzerTileDefinitions.THREE_DIMENSIONAL_SCENE)
-    .render(() => (<ThreeTutorial/>))
+    .render(() => (
+        <Suspense fallback={null}>
+            <ThreeTutorial />
+        </Suspense>
+    ))
     .build()
 
-const ActiveExampleProvider = ({children}) => {
-
+const ActiveExampleProvider = ({ children }) => {
     const [activeExample, setActiveExample] = useState<number | null>(1)
 
     console.log("provider render")
     return (
-        <ActiveExampleContext.Provider value={{activeExample, setActiveExample}}>{children}</ActiveExampleContext.Provider>
+        <ActiveExampleContext.Provider value={{ activeExample, setActiveExample }}>
+            {children}
+        </ActiveExampleContext.Provider>
     )
-
 }
 
 const root = createRoot(document.getElementById("root"))
@@ -232,11 +220,11 @@ root.render(
                         GlowbuzzerTileDefinitions.CONFIG_EDIT,
                         ChooseExampleTileDefinition,
                         GlowbuzzerTileDefinitions.TOOLS,
-                        CustomSceneTile,
+                        CustomSceneTile
                     ]}
                 >
-                    <ExampleAppMenu title="three-tutorial"/>
-                    <DockLayout/>
+                    <ExampleAppMenu title="three-tutorial" />
+                    <DockLayout />
                 </DockLayoutProvider>
             </ActiveExampleProvider>
         </GlowbuzzerApp>
