e6d2a30dcbc361dd50a4ce845036ee4695788ed6
jugglingcats <github@akirkpatrick.com>
2022-12-02 14:09:06 +0000

    fix: add back in GlowbuzzerTileDefinitionList.ts for full list of tiles

---
+++ libs/controls/src/GlowbuzzerTileDefinitionList.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2022. Glowbuzzer. All rights reserved
+ */
+
+import { CartesianDroTileDefinition, JointDroTileDefinition } from "./dro"
+import { CartesianJogTileDefinition, JointJogTileDefinition } from "./jogging"
+import { ConnectTileDefinition } from "./connect"
+import { ConfigEditTileDefinition } from "./config"
+import { ThreeDimensionalSceneTileDefinition } from "./scene"
+import { GCodeTileDefinition } from "./gcode"
+import { FeedRateTileDefinition } from "./feedrate"
+import { SpindleTileDefinition } from "./spindle"
+import {
+    AnalogInputsTileDefinition,
+    AnalogOutputsTileDefinition,
+    DigitalInputsTileDefinition,
+    DigitalOutputsTileDefinition,
+    IntegerInputsTileDefinition,
+    IntegerOutputsTileDefinition
+} from "./io"
+import { FramesTileDefinition } from "./frames"
+import { PointsTileDefinition } from "./points"
+import { ToolsTileDefinition } from "./tools"
+import { TasksTileDefinition } from "./tasks"
+import { TelemetryTileDefinition } from "./telemetry"
+import { StateMachineToolsTileDefinition } from "./dev"
+
+/**
+ * This is the complete list of glowbuzzer tile definitions. Be aware that if you import this list
+ * into your project it will transitively import all of the glowbuzzer controls code, and all dependencies.
+ * If you are only using a handful of tiles, you may want to import only the tiles you need.
+ */
+export const GlowbuzzerTileDefinitionList = [
+    CartesianDroTileDefinition,
+    JointDroTileDefinition,
+    CartesianJogTileDefinition,
+    JointJogTileDefinition,
+    ConnectTileDefinition,
+    ConfigEditTileDefinition,
+    ThreeDimensionalSceneTileDefinition,
+    GCodeTileDefinition,
+    FeedRateTileDefinition,
+    SpindleTileDefinition,
+    DigitalInputsTileDefinition,
+    AnalogInputsTileDefinition,
+    IntegerInputsTileDefinition,
+    DigitalOutputsTileDefinition,
+    AnalogOutputsTileDefinition,
+    IntegerOutputsTileDefinition,
+    FramesTileDefinition,
+    PointsTileDefinition,
+    ToolsTileDefinition,
+    TasksTileDefinition,
+    TelemetryTileDefinition,
+    StateMachineToolsTileDefinition
+]
