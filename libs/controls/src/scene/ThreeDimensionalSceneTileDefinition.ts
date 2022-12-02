01c59201b09e98e6abb60bd9e52cae4ec88bb2bc
jugglingcats <github@akirkpatrick.com>
2022-12-02 14:55:36 +0000

    fix: roll back rename of ThreeDimensionalSceneTileDefinition.ts

--- libs/controls/src/scene/ThreeDimensionalSceneTileDefinition.ts
+++ libs/controls/src/scene/ThreeDimensionalSceneTileDefinition.ts
@@ -2,13 +2,20 @@
  * Copyright (c) 2022. Glowbuzzer. All rights reserved
  */
 
-import * as React from "react"
 import { GlowbuzzerTileIdentifiers } from "../GlowbuzzerTileIdentifiers"
 import { createElement } from "react"
 import { ThreeDimensionalSceneTileHelp } from "./ThreeDimensionalSceneTileHelp"
 import { ThreeDimensionalSceneTile } from "./ThreeDimensionalSceneTile"
 import { TrackPosition } from "./TrackPosition"
 import { Frustum } from "./Frustum"
+
+function renderThreeDimensionalSceneWithFrustum() {
+    return createElement(
+        ThreeDimensionalSceneTile,
+        {},
+        createElement(TrackPosition, { kinematicsConfigurationIndex: 0 }, createElement(Frustum))
+    )
+}
 
 export const ThreeDimensionalSceneTileDefinition = {
     id: GlowbuzzerTileIdentifiers.THREE_DIMENSIONAL_SCENE,
@@ -18,13 +25,7 @@ export const ThreeDimensionalSceneTileDefinition = {
         column: 1,
         row: 0
     },
-    render: () => (
-        <ThreeDimensionalSceneTile>
-            <TrackPosition kinematicsConfigurationIndex={0}>
-                <Frustum />
-            </TrackPosition>
-        </ThreeDimensionalSceneTile>
-    ),
+    render: () => renderThreeDimensionalSceneWithFrustum(),
     renderHelp: () => createElement(ThreeDimensionalSceneTileHelp, {}, null),
     config: {
         enableWithoutConnection: true
