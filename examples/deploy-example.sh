264775f40d0774f9f8ca79e7f55d0b8e0bd78021
jugglingcats <github@akirkpatrick.com>
2023-04-21 16:37:39 +0100

    fix: fix examples build

--- examples/deploy-example.sh
+++ examples/deploy-example.sh
@@ -3,5 +3,5 @@
 #
 # Copyright (c) 2022. Glowbuzzer. All rights reserved
 #
-set example=`basename $1`
+example=$(basename $1)
 aws s3 sync --region eu-west-1 --delete ./examples/$1/dist "s3://$2/$example"
