7631ce2fef74b6519d77cb6e61fa3e1b04f41705
jugglingcats <github@akirkpatrick.com>
2023-06-06 13:05:44 +0100

    chore: move cylinder virtual position logic into store extraReducers

--- examples/applications/convmc/src/components/PneumaticCylinder.tsx
+++ examples/applications/convmc/src/components/PneumaticCylinder.tsx
@@ -2,30 +2,19 @@
  * Copyright (c) 2023. Glowbuzzer. All rights reserved
  */
 
-import { Mesh, Vector3 } from "three"
-import React, { useEffect, useMemo, useRef } from "react"
-import { useFrame, useThree } from "@react-three/fiber"
+import { Vector3 } from "three"
+import React, { useMemo } from "react"
 import { useGLTF } from "@react-three/drei"
-import { appSlice, useAppState } from "../store"
-import { useDispatch } from "react-redux"
-import { useDigitalOutputState } from "@glowbuzzer/store"
-import { DigitalOutput } from "../constants"
+import { useAppState } from "../store"
 
 const POSITION_IN_SCENE = new Vector3(0.1, -0.1, 0.35)
 const ROD_INITIAL_POSITION = new Vector3(0.031, 0, 0.006)
 
 /**
- * Pneumatic pusher component. Reacts to the cylinder trigger (digital output), and animates the cylinder position.
- * The cylinder position is dispatched to the Redux store, because it is used by the conveyor belt animation to determine
- * the position of the object on the conveyor when it is being pushed.
+ * Pneumatic pusher component, which updates based on the virtual cylinder position from the app state.
  */
 export const PneumaticCylinder = () => {
-    const [{ effectiveValue: cylinderTriggered }] = useDigitalOutputState(DigitalOutput.CYLINDER)
-    const dispatch = useDispatch()
-    const cylinderPosition = useRef(0)
-    const cylinderStepDirection = useRef(0)
-    const rodRef = useRef<Mesh>()
-    const { invalidate } = useThree()
+    const { cylinderPosition } = useAppState()
 
     const [cylinder, rod] = useMemo(
         () =>
@@ -39,46 +28,14 @@ export const PneumaticCylinder = () => {
         []
     )
 
-    useFrame(state => {
-        // if we're currently stepping in/out the cylinder, do the updates
-        if (cylinderStepDirection.current !== 0) {
-            let new_position = cylinderPosition.current + cylinderStepDirection.current
-            if (new_position < 0) {
-                new_position = 0
-                cylinderStepDirection.current = 0
-            } else if (new_position > 0.1) {
-                new_position = 0.1
-                cylinderStepDirection.current = 0
-            }
-            // update scene
-            rodRef.current.position.setY(new_position)
-            // update store
-            dispatch(appSlice.actions.setCylinderPosition(new_position))
-            // update local state
-            cylinderPosition.current = new_position
-            // ensure we get a new animation frame (frameloop="demand")
-            invalidate()
-        }
-    })
-
-    useEffect(() => {
-        // if the triggered state has changed, update the step/direction of the movement
-        if (cylinderTriggered && cylinderPosition.current < 0.1) {
-            cylinderStepDirection.current = 0.01
-            // we should get a re-render anyway, but just in case
-            invalidate()
-        } else if (!cylinderTriggered && cylinderPosition.current > 0) {
-            cylinderStepDirection.current = -0.01
-            invalidate()
-        } else {
-            cylinderStepDirection.current = 0
-        }
-    }, [cylinderTriggered])
+    const position = useMemo(() => {
+        return ROD_INITIAL_POSITION.clone().setY(cylinderPosition)
+    }, [cylinderPosition])
 
     return (
         <group position={POSITION_IN_SCENE}>
             <primitive object={cylinder} />
-            <primitive ref={rodRef} object={rod} position={ROD_INITIAL_POSITION} />
+            <primitive object={rod} position={position} />
         </group>
     )
 }
--- examples/applications/convmc/src/store.ts
+++ examples/applications/convmc/src/store.ts
@@ -4,7 +4,8 @@
 
 import { combineReducers, createSlice } from "@reduxjs/toolkit"
 import { useSelector } from "react-redux"
-import { StateMachineDefinition } from "@glowbuzzer/store"
+import { digitalOutputsSlice, machineSlice, StateMachineDefinition } from "@glowbuzzer/store"
+import { DigitalOutput } from "./constants"
 
 export enum CylinderAction {
     NONE,
@@ -68,9 +69,6 @@ export const appSlice = createSlice({
         setObjectInitialPosition(state, action) {
             state.objectInitialPosition = action.payload
         },
-        setCylinderPosition(state, action) {
-            state.cylinderPosition = action.payload
-        },
         setDetectedObjectType(state, action) {
             state.detectedObjectType = action.payload
         },
@@ -80,6 +78,49 @@ export const appSlice = createSlice({
         setJobEnabled(state, action) {
             state.jobEnabled = action.payload
         }
+    },
+    extraReducers: builder => {
+        // we want to produce a virtual cylinder positoin based on whether the cylinder is triggered or not
+        let cylinderPosition = 0
+        let cylinderStepDirection = 0
+        let lastHeartbeat = 0
+        builder.addCase(digitalOutputsSlice.actions.status, (state, action) => {
+            const heartbeat = action.payload.heartbeat
+            // don't do anything if we haven't received a heartbeat yet
+            if (lastHeartbeat > 0) {
+                // the delta is used to control the speed of the pusher
+                const delta = heartbeat - lastHeartbeat
+
+                // are we triggered?
+                const cylinderTriggered =
+                    action.payload.status[DigitalOutput.CYLINDER].effectiveValue
+
+                // update the direction based on triggered / not triggered
+                if (cylinderTriggered && cylinderPosition < 0.1) {
+                    cylinderStepDirection = 1
+                } else if (!cylinderTriggered && cylinderPosition > 0) {
+                    cylinderStepDirection = -1
+                } else {
+                    cylinderStepDirection = 0
+                }
+
+                // if we're currently stepping in/out the cylinder, update the state
+                if (cylinderStepDirection !== 0) {
+                    let new_position = cylinderPosition + cylinderStepDirection * delta * 0.001
+                    if (new_position < 0) {
+                        new_position = 0
+                        cylinderStepDirection = 0
+                    } else if (new_position > 0.1) {
+                        new_position = 0.1
+                        cylinderStepDirection = 0
+                    }
+                    cylinderPosition = new_position
+                }
+                // finally make the position available in the app state
+                state.cylinderPosition = cylinderPosition
+            }
+            lastHeartbeat = heartbeat
+        })
     }
 })
 
