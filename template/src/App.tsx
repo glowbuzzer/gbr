9fbdcea5512872337d10e1207c34db343f2355b0
jugglingcats <github@akirkpatrick.com>
2023-07-31 14:03:12 +0100

    fix(#108): ensure style-components version match in glowbuzzer template

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,18 +1,24 @@
-import {DockLayout, DockLayoutProvider, GlowbuzzerTileDefinitionList, useDockViewMenu} from "@glowbuzzer/controls";
-import {Menu} from "antd";
-import * as React from "react";
+import {
+    DockLayout,
+    DockLayoutProvider,
+    GlowbuzzerTileDefinitionList,
+    useDockViewMenu
+} from "@glowbuzzer/controls"
+import { Menu } from "antd"
+import * as React from "react"
 
 const AppMenu = () => {
     const viewMenu = useDockViewMenu()
-    return <Menu mode="horizontal" selectedKeys={[]} items={[viewMenu]}/>
+    return <Menu mode="horizontal" selectedKeys={[]} items={[viewMenu]} />
 }
 
 export const App = () => {
-    return <>
-        <DockLayoutProvider tiles={GlowbuzzerTileDefinitionList}>
-            <AppMenu/>
-            <DockLayout/>
-        </DockLayoutProvider>
-    </>
-
+    return (
+        <>
+            <DockLayoutProvider tiles={GlowbuzzerTileDefinitionList}>
+                <AppMenu />
+                <DockLayout />
+            </DockLayoutProvider>
+        </>
+    )
 }
