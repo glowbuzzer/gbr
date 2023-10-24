310d1c5652591435238303f4942319ee19a45945
jugglingcats <github@akirkpatrick.com>
2023-10-24 11:32:47 +0100

    chore: remove fieldbus config from aw-l20 and update gbc.ts

--- vendors/automationware/lib/awtube/config.ts
+++ vendors/automationware/lib/awtube/config.ts
@@ -34,44 +34,6 @@ export const StandardAwTubeConfiguration: GlowbuzzerConfig = {
         {
             name: "default"
         }
-    ],
-    fieldbus: [
-        {
-            name: "0",
-            jointCount: 10,
-            RxPdo: {
-                machineStatusWordOffset: 0,
-                activeFaultOffset: 4,
-                faultHistoryOffset: 8,
-                heartbeatOffset: 12,
-                jointStatuswordOffset: 16,
-                jointActualPositionOffset: 36,
-                jointActualVelocityOffset: 76,
-                jointActualTorqueOffset: 116,
-                digitalOffset: 156,
-                digitalCount: 8,
-                analogOffset: 164,
-                analogCount: 6,
-                integerOffset: 188,
-                integerCount: 2
-            },
-            TxPdo: {
-                machineControlWordOffset: 0,
-                gbcControlWordOffset: 4,
-                hlcControlWordOffset: 8,
-                heartbeatOffset: 12,
-                jointControlwordOffset: 16,
-                jointSetPositionOffset: 36,
-                jointSetVelocityOffset: 76,
-                jointSetTorqueOffset: 116,
-                digitalOffset: 156,
-                digitalCount: 10,
-                analogOffset: 164,
-                analogCount: 6,
-                integerOffset: 188,
-                integerCount: 2
-            }
-        }
     ]
 }
 
