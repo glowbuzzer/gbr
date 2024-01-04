e1cd27a51b81acd1c77dc91ccfd07a994f8f2d9b
jugglingcats <github@akirkpatrick.com>
2024-01-04 14:12:06 +0000

    chore: remove defunct lib folder

--- vendors/automationware/lib/awtube/index.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (c) 2023. Glowbuzzer. All rights reserved
- */
-
-export * from "./types"
-export * from "./scene"
--- vendors/automationware/lib/awtube/scene/index.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (c) 2023. Glowbuzzer. All rights reserved
- */
-
-export * from "./AwTubeRobot"
-export * from "./parts"
