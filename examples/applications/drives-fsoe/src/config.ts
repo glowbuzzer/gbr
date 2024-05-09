dc16e4adc07d212bc857d7be071c6fac9398a252
David <david@glowbuzzer.com>
2024-05-09 09:09:58 +0100

    chore: Update J25 params

--- examples/applications/drives-fsoe/src/config.ts
+++ examples/applications/drives-fsoe/src/config.ts
@@ -6,7 +6,8 @@ import {
     DIN_SAFETY_TYPE,
     GlowbuzzerConfig,
     JOINT_MODEOFOPERATION,
-    JOINT_TORQUE_MODE
+    JOINT_TORQUE_MODE,
+    KC_KINEMATICSCONFIGURATIONTYPE
 } from "@glowbuzzer/store"
 
 export const config: GlowbuzzerConfig = {
@@ -28,7 +29,7 @@ export const config: GlowbuzzerConfig = {
             ],
             scalePos: 166886,
             scaleVel: 9549,
-            scaleTorque: 32.68,
+            scaleTorque: 4.3,
             jointType: 1,
             pidConfig: [
                 {
@@ -40,6 +41,7 @@ export const config: GlowbuzzerConfig = {
                     sampleTime: 20
                 }
             ],
+            dynamicsVelocityThreshold: 0.02,
             preferredMode: JOINT_MODEOFOPERATION.JOINT_MODEOFOPERATION_CSP,
             supportedModes:
                 JOINT_MODEOFOPERATION.JOINT_MODEOFOPERATION_CSP |
@@ -93,6 +95,7 @@ export const config: GlowbuzzerConfig = {
         {
             name: "default",
             participatingJointsCount: 3,
+            kinematicsConfigurationType: KC_KINEMATICSCONFIGURATIONTYPE.KC_NAKED,
             participatingJoints: [0, 1, 2],
             linearLimits: [
                 {
@@ -102,7 +105,7 @@ export const config: GlowbuzzerConfig = {
                 }
             ],
             inverseDynamicParams: [
-                { jointInertia: 0.94, damping: 2.67, friction: 2.4 },
+                { jointInertia: 4.0, damping: 25.0, friction: 16.0 },
                 { jointInertia: 1.2, damping: 2.2, friction: 3.2 },
                 { jointInertia: 1.3, damping: 2.3, friction: 3.3 }
             ]
@@ -110,48 +113,71 @@ export const config: GlowbuzzerConfig = {
     ],
     safetyDin: [
         {
-            name: "Overall safety state (1)",
-            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_HIDDEN
+            name: "Overall safety state (0)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_OVERALL_STATE
         },
         {
-            name: "Safety error state (2)",
-            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_HIDDEN
+            name: "Safety error state (1)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_RESERVED
         },
         {
-            name: "Restart acknowledge state (3)",
-            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_HIDDEN
+            name: "Restart acknowledge state (2)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_RESERVED
         },
         {
-            name: "Drive - STO (4)"
+            name: "Drive - STO (3)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_ACKNOWLEDGEABLE
         },
         {
-            name: "Drive - SOS (5)"
+            name: "Drive - SOS (4)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_ACKNOWLEDGEABLE
         },
         {
-            name: "Drive - SS1 (6)"
+            name: "Drive - SS1 (5)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_ACKNOWLEDGEABLE
         },
         {
-            name: "Drive - SS2 (7)"
+            name: "Drive - SS2 (6)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_ACKNOWLEDGEABLE
         },
         {
-            name: "Drive - SLS1 (8)"
+            name: "Drive - SLS1 (7)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_ACKNOWLEDGEABLE
         },
         {
-            name: "Drive - SBC (9)"
+            name: "Drive - SBC (8)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_ACKNOWLEDGEABLE
         },
         {
-            name: "Drive - Over temperature (10)"
+            name: "Drive - Over temperature (9)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_UNACKNOWLEDGEABLE
+        },
+        {
+            name: "Drive - Safe pos valid (10)",
+            type: DIN_SAFETY_TYPE.DIN_SAFETY_TYPE_SAFE_POS_VALID
+        },
+        {
+            name: "Safety PLC - CW SLP (11)"
+        },
+        {
+            name: "Safety PLC - CCW SLP (12)"
         }
     ],
     safetyDout: [
-        {
-            name: "Restart Acknowledge (cmd) (0)"
-        },
         {
             name: "Control alive (cmd) (1)"
         },
         {
             name: "Drive SS1 (cmd) (2)"
+        },
+        {
+            name: "Drive SS0 (cmd) (3)"
+        },
+        {
+            name: "Drive SS2 (cmd) (4)"
+        },
+        {
+            name: "Drive Reset Pos (cmd) (5)"
         }
     ]
 }
