bd0f2af83d1f09657d010dc3a90f33cef15cbd2c
David <david@glowbuzzer.com>
2024-06-24 14:12:12 +0100

    chore: Add virtual hmi prototype

--- examples/applications/drives-fsoe/src/tiles.ts
+++ examples/applications/drives-fsoe/src/tiles.ts
@@ -5,10 +5,18 @@
 import { DockTileDefinitionBuilder } from "@glowbuzzer/controls"
 import React from "react"
 import { DrivesMoveTile } from "./DrivesMoveTile"
+import { VirtualHmiTile } from "./VirtualHmiTile"
 
 export const DrivesOscillatingMoveTileDefinition = DockTileDefinitionBuilder()
     .id("drives-oscillating-move")
     .name("Move Drives")
     .placement(0, 1)
     .render(() => React.createElement(DrivesMoveTile))
+    .build()
+
+export const VirtualHmiTileDefinition = DockTileDefinitionBuilder()
+    .id("virtual-hmi")
+    .name("Virtual HMI")
+    .placement(0, 2)
+    .render(() => React.createElement(VirtualHmiTile))
     .build()
