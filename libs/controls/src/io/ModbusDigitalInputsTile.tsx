2f124ee2373169f20f4dceb3fa2631cd4a63db09
David <david@glowbuzzer.com>
2024-06-24 14:09:36 +0100

    chore: Update io test tiles

--- libs/controls/src/io/ModbusDigitalInputsTile.tsx
+++ libs/controls/src/io/ModbusDigitalInputsTile.tsx
@@ -9,24 +9,10 @@ import {
     useModbusUnsignedIntegerInputState
 } from "@glowbuzzer/store"
 import { StyledTileContent } from "../util/styles/StyledTileContent"
-import { Tag } from "antd"
+import { Tag, Tooltip } from "antd"
 
 import styled from "styled-components"
 import { ModbusErrorCodeString } from "./ModbusErrorCodeString"
-
-const StyledDigitalInput = styled.div`
-    display: flex;
-    padding: 1px 0;
-
-    .label {
-        flex-grow: 1;
-    }
-
-    .ant-tag {
-        width: 40px;
-        text-align: center;
-    }
-`
 
 type ModbusDigitalInputsTileProps = {
     /**
@@ -41,13 +27,15 @@ const StyledDiv = styled.div`
     align-items: center;
 `
 
-const ModbusDigitalInputItem = ({ label, index }) => {
+const ModbusDigitalInputItem = ({ label, description, index }) => {
     const dinStatus = useModbusDigitalInputState(index)
     // console.log("dinStatus", dinStatus)
     if (!dinStatus) {
         return (
             <StyledDiv key={index}>
-                <div>{label}</div>
+                <Tooltip title={description}>
+                    <div>{label}</div>
+                </Tooltip>
                 <div>
                     <Tag>No Data Available</Tag>
                 </div>
@@ -59,9 +47,11 @@ const ModbusDigitalInputItem = ({ label, index }) => {
 
     return (
         <StyledDiv key={index}>
-            <div>{label}</div>
+            <Tooltip title={description}>
+                <div>{label}</div>
+            </Tooltip>
             <div>
-                Value: <Tag color={actValue ? "green" : "red"}>{actValue ? "ON" : "OFF"}</Tag>
+                <Tag color={actValue ? "green" : "red"}>{actValue ? "ON" : "OFF"}</Tag>
                 {isError && (
                     <>
                         Error Code:{" "}
@@ -75,30 +65,30 @@ const ModbusDigitalInputItem = ({ label, index }) => {
     )
 }
 
-type ModbusIntegerInputsTileProps = {
-    /**
-     * Override labels to use for inputs, in the order given in the configuration
-     */
-    labels?: string[]
-}
-
 /**
  * The modbus digital inputs tile shows a simple view of all current digital input values.
  */
 export const ModbusDigitalInputsTile = ({ labels = [] }: ModbusDigitalInputsTileProps) => {
     const dins = useModbusDigitalInputList()
 
-    // console.log("dins", dins)
-
     return (
         <StyledTileContent>
-            {dins?.map(({ name }, index) => (
-                <ModbusDigitalInputItem
-                    key={index}
-                    index={index}
-                    label={labels[index] || name || index}
-                />
-            ))}
+            {dins?.map((din, index) => {
+                const description = `Slave: ${din.slave_num}, Address: ${din.address}, Function: ${
+                    din.function
+                }, Little Endian: ${din.little_endian ? "Yes" : "No"}, Inverted: ${
+                    din.inverted ? "Yes" : "No"
+                }`
+
+                return (
+                    <ModbusDigitalInputItem
+                        key={index}
+                        index={index}
+                        label={labels[index] || din.name || `Input ${index}`}
+                        description={description} // Passing description
+                    />
+                )
+            })}
         </StyledTileContent>
     )
 }
--- libs/controls/src/io/SafetyDigitalOutputsTileDefinition.ts
+++ libs/controls/src/io/SafetyDigitalOutputsTileDefinition.ts
@@ -2,10 +2,10 @@
  * Copyright (c) 2022. Glowbuzzer. All rights reserved
  */
 
-import {GlowbuzzerTileIdentifiers} from "../GlowbuzzerTileIdentifiers"
-import {createElement} from "react"
-import {SafetyDigitalOutputsTile} from "./SafetyDigitalOutputsTile"
-import {SafetyDigitalOutputsTileHelp} from "./SafetyDigitalOutputsTileHelp"
+import { GlowbuzzerTileIdentifiers } from "../GlowbuzzerTileIdentifiers"
+import { createElement } from "react"
+import { SafetyDigitalOutputsTile } from "./SafetyDigitalOutputsTile"
+import { SafetyDigitalOutputsTileHelp } from "./SafetyDigitalOutputsTileHelp"
 
 export const SafetyDigitalOutputsTileDefinition = {
     id: GlowbuzzerTileIdentifiers.SAFETY_DIGITAL_OUTPUTS,
@@ -14,6 +14,7 @@ export const SafetyDigitalOutputsTileDefinition = {
         column: 2,
         row: 2
     },
+    enableWithoutConnection: true,
     render: () => createElement(SafetyDigitalOutputsTile, {}, null),
     renderHelp: () => createElement(SafetyDigitalOutputsTileHelp, {}, null)
 }
