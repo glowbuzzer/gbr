811ba1a54117374af60f6bad254e83e34fb2a2b9
jugglingcats <github@akirkpatrick.com>
2024-08-19 08:25:18 +0100

    chore: add way to put safety inputs into 'good' state in sim mode

--- libs/controls/src/status/StatusBarLiveSwitch.tsx
+++ libs/controls/src/status/StatusBarLiveSwitch.tsx
@@ -8,24 +8,77 @@ import {
     useConnection,
     useLiveModeEnabled,
     useMachine,
+    useMachineConfig,
+    useSafetyDigitalInputList,
     useSimilationOnlyConfiguration
 } from "@glowbuzzer/store"
-import { Button } from "antd"
+import { Button, Dropdown, MenuProps } from "antd"
 
 export const StatusBarLiveSwitch = () => {
     const simulationOnly = useSimilationOnlyConfiguration()
-    const { connected } = useConnection()
+    const { connected, send } = useConnection()
     const live_switch_enabled = useLiveModeEnabled()
     const machine = useMachine()
+    const safetyInputs = useSafetyDigitalInputList()
+    const machineConfig = useMachineConfig()
 
+    const supports_safety = !!machineConfig.$metadata?.safetyStateInput
     const disabled = simulationOnly || !connected
 
     function change_target(v: MACHINETARGET) {
         machine.setDesiredMachineTarget(v)
     }
 
+    function override_safety_state_simulated() {
+        // we're going to force all inputs into the non-negative state
+        const overrides = Object.fromEntries(
+            safetyInputs.map((input, index) => {
+                return [
+                    index,
+                    {
+                        command: {
+                            setValue: input.$metadata?.negativeState ? 0 : 1, // non-negative state
+                            override: true
+                        }
+                    }
+                ]
+            })
+        )
+
+        send(
+            JSON.stringify({
+                command: {
+                    safetyDin: {
+                        ...overrides
+                    }
+                }
+            })
+        )
+    }
+
     const live = machine.requestedTarget === MACHINETARGET.MACHINETARGET_FIELDBUS
     const switching = connected && machine.target !== machine.requestedTarget
+
+    const sim_button_props = {
+        type: live ? undefined : ("primary" as "primary"),
+        loading: !live && switching,
+        onClick() {
+            change_target(MACHINETARGET.MACHINETARGET_SIMULATION)
+        },
+        disabled: disabled,
+        menu: supports_safety
+            ? {
+                  items: [
+                      {
+                          key: "make_safe",
+                          label: "Simulate Required Safety Inputs",
+                          onClick: override_safety_state_simulated
+                      }
+                  ]
+              }
+            : undefined
+    }
+    const SimButton = supports_safety ? Dropdown.Button : Button
 
     return (
         <Button.Group>
@@ -37,14 +90,7 @@ export const StatusBarLiveSwitch = () => {
             >
                 Normal
             </Button>
-            <Button
-                type={live ? undefined : "primary"}
-                loading={!live && switching}
-                onClick={() => change_target(MACHINETARGET.MACHINETARGET_SIMULATION)}
-                disabled={disabled}
-            >
-                Simulation
-            </Button>
+            <SimButton {...sim_button_props}>Simulation</SimButton>
         </Button.Group>
     )
 }
